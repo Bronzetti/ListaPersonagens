29225f06e5ec7d9f20c689d42f30ea5120601966
Bronzetti <68982370+Bronzetti@users.noreply.github.com>
2021-04-10 19:36:18 -0300

    Update

--- app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
@@ -2,6 +2,7 @@ package com.example.listapersonagens.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.listapersonagens.R;
+import com.example.listapersonagens.dao.PersonagemDAO;
 import com.example.listapersonagens.model.Personagem;
 
 public class FormularioPersonagemActivity extends AppCompatActivity {
@@ -18,8 +20,12 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_personagem);
+        setTitle("Formulário de Personagens"); //título ao ir para o formulário
 
-       //vincula as informações das variáveis no meu formulário com os objetos dentro do xml
+        PersonagemDAO dao = new PersonagemDAO();
+
+
+        //vincula as informações das variáveis no meu formulário com os objetos dentro do xml
         EditText campoNome = findViewById(R.id.edittext_nome);
         EditText campoAltura= findViewById(R.id.edittext_altura);
         EditText campoNascimento = findViewById(R.id.edittext_nascimento);
@@ -33,15 +39,21 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
                 String nome = campoNome.getText().toString(); //pega a variável nome e com o getText, pegará a informação passada dentro desse campo
                 String altura = campoAltura.getText().toString();
                 String nascimento = campoNascimento.getText().toString();
+
                 //criação de um construtor para armazenar as variáveis locais de forma mais segura
                 Personagem personagemSalvo = new Personagem(nome, altura, nascimento);
 
+                dao.salvar(personagemSalvo);
 
-                Toast.makeText(FormularioPersonagemActivity.this,
+                startActivity(new Intent(FormularioPersonagemActivity.this, ListaPersonagemActivity.class));
+
+
+
+                /*Toast.makeText(FormularioPersonagemActivity.this,
                         personagemSalvo.getNome() + " - " +
                                 personagemSalvo.getAltura() + " - " +
                                 personagemSalvo.getNascimento(), Toast.LENGTH_SHORT).show();
-
+*/
                 new Personagem(nome, altura, nascimento);
 
                 //Toast.makeText(FormularioPersonagemActivity.this,"Estou Funcionando!",Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/listapersonagens/ui/activities/ListaPersonagemActivity.java
+++ app/src/main/java/com/example/listapersonagens/ui/activities/ListaPersonagemActivity.java
@@ -1,13 +1,17 @@
 
 package com.example.listapersonagens.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.listapersonagens.R;
+import com.example.listapersonagens.dao.PersonagemDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,10 +24,22 @@ public class ListaPersonagemActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstancesState){
   super.onCreate(savedInstancesState);
   setContentView(R.layout.activity_lista_personagem);
-  List<String> personagem = new ArrayList<>(Arrays.asList("Alex","Ken", "Ryu","Guile")); //criação de uma lista com o nome dos personagens
+  setTitle("Lista de Personagens"); //título ao entrar na lista de personagens
+
+  PersonagemDAO dao = new PersonagemDAO();
+
+  //List<String> personagem = new ArrayList<>(Arrays.asList("Alex","Ken", "Ryu","Guile")); //criação de uma lista com o nome dos personagens
+
+  FloatingActionButton botaoNovoPersonagem = findViewById(R.id.fab_add);//botão que leva da lista para o formulário
+  botaoNovoPersonagem.setOnClickListener(new View.OnClickListener() {//chamar ação do botão
+    @Override
+    public void onClick(View v) { //toda vez que apertar o fab irá direto para o formulário
+       startActivity(new Intent(ListaPersonagemActivity.this, FormularioPersonagemActivity.class));
+    }
+  });
 
   ListView listaDePersonagens = findViewById(R.id.activity_main_lista_personagem); //pega a lista através do id
-listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, personagem));
+  listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
 
        /* TextView primeiroPersonagem = findViewById(R.id.textView); //
         TextView segundoPersonagem = findViewById(R.id.textView2);
